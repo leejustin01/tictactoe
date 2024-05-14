8b1b0bd684ff5f242c6676ed4982b69217091cd0
leejustin01 <justlee0308@gmail.com>
2024-05-14 16:40:16 -0700

    Started on building components for tictactoe game

---
+++ tictactoeREACT/src/components/Tile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FaRegCircle as circle, ImCross as cross } from "react-icons/fa";
+
+function Tile({ onClick }) {
+    return (
+        <td></td>
+    )
+}
+
+
+
+export default Tile;
